37bea559bdff2f5c244f44dc10db0c81cd6ef3de
Natalia Carvalho <ssspinosaure@gmail.com>
2023-03-06 14:57:42 -0300

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chocolate: "#441a21",
+        imperial: "#F54746",
+        isabeline: "#EDE4E0",
+        champagne: "#F4E6CC",
+        melon: "#FEAD96",
+      },
+    },
   },
   plugins: [],
 };
